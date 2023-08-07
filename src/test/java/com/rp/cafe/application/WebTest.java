c3dacfe08cf928161bee9b251d376d32f322c4eb
jianliangh <rpdeveloped@gmail.com>
2023-08-07 19:38:08 +0800

    Update

---
+++ src/test/java/com/rp/cafe/application/WebTest.java
@@ -0,0 +1,160 @@
+/**
+ * 
+ */
+package com.rp.cafe.application;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author hp
+ *
+ */
+class WebTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link com.rp.cafe.application.AccountController#page(org.springframework.ui.Model, java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	void testPage() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.rp.cafe.application.AccountController#set(org.springframework.ui.Model, javax.servlet.http.HttpSession, java.security.Principal)}.
+	 */
+	@Test
+	void testSet() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.rp.cafe.application.AccountController#home(org.springframework.ui.Model, javax.servlet.http.HttpSession)}.
+	 */
+	@Test
+	void testHome() {
+		assertEquals("right" , "right");
+	}
+
+	/**
+	 * Test method for {@link com.rp.cafe.application.AccountController#login(org.springframework.ui.Model)}.
+	 */
+	@Test
+	void testLogin() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.rp.cafe.application.AccountController#account(org.springframework.ui.Model, javax.servlet.http.HttpSession)}.
+	 */
+	@Test
+	void testAccount() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.rp.cafe.application.AccountController#saveUser(com.rp.cafe.application.model.user.Users, org.springframework.ui.Model, javax.servlet.http.HttpSession)}.
+	 */
+	@Test
+	void testSaveUser() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.rp.cafe.application.AccountController#dashboard(org.springframework.ui.Model, javax.servlet.http.HttpSession)}.
+	 */
+	@Test
+	void testDashboard() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.rp.cafe.application.AccountController#password(org.springframework.ui.Model, javax.servlet.http.HttpSession)}.
+	 */
+	@Test
+	void testPassword() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.rp.cafe.application.AccountController#passwordchange(java.lang.String, org.springframework.ui.Model, javax.servlet.http.HttpSession)}.
+	 */
+	@Test
+	void testPasswordchange() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.rp.cafe.application.AccountController#cardlink(org.springframework.ui.Model, javax.servlet.http.HttpSession)}.
+	 */
+	@Test
+	void testCardlink() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.rp.cafe.application.AccountController#saveCard(com.rp.cafe.application.model.card.Card, org.springframework.ui.Model, javax.servlet.http.HttpSession)}.
+	 */
+	@Test
+	void testSaveCard() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.rp.cafe.application.AccountController#cardview(org.springframework.ui.Model, javax.servlet.http.HttpSession)}.
+	 */
+	@Test
+	void testCardview() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.rp.cafe.application.AccountController#topup(org.springframework.ui.Model, javax.servlet.http.HttpSession)}.
+	 */
+	@Test
+	void testTopup() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.rp.cafe.application.AccountController#addCardBalance(long, org.springframework.ui.Model, javax.servlet.http.HttpSession)}.
+	 */
+	@Test
+	void testAddCardBalance() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.rp.cafe.application.AccountController#tranview(org.springframework.ui.Model, javax.servlet.http.HttpSession)}.
+	 */
+	@Test
+	void testTranview() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.rp.cafe.application.AccountController#transort(java.lang.String, org.springframework.ui.Model, javax.servlet.http.HttpSession)}.
+	 */
+	@Test
+	void testTransort() {
+		fail("Not yet implemented");
+	}
+
+}
